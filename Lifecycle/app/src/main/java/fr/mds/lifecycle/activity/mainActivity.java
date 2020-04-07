30d6cd96b279b1927b282f4786f52ba058d23954
daproclaima <sn.itpro@gmail.com>
2020-04-07 10:10:54 +0200

    adds LifeCycle project

--- Lifecycle/app/src/main/java/fr/mds/lifecycle/activity/mainActivity.java
+++ Lifecycle/app/src/main/java/fr/mds/lifecycle/activity/mainActivity.java
@@ -1,5 +1,6 @@
 package fr.mds.lifecycle.activity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -8,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import fr.mds.lifecycle.R;
 
-public class mainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "Lifecycle";
 
@@ -16,38 +17,47 @@ public class mainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
-        this.onStart();
-        this.onPause();
-        this.onResume();
-        this.onStop();
-        this.onRestart();
-        this.onStop();
         Log.d(TAG, "LifeCycleActivity");
+        Resources resources = getResources();
+        String helloWorldStr = resources.getString(R.string.hello_string);
+        Log.d(TAG,helloWorldStr );
+
+//        String cancelAndroidString = resources.getString(android.R.string.cancel);
+//        Log.d(TAG, cancelAndroidString);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.d(TAG, "MainActivity - onStart");
     }
 
     @Override
     protected void onStop() {
+
         super.onStop();
+        Log.d(TAG, "MainActivity - onStop");
     }
 
     @Override
     protected void onResume() {
+
         super.onResume();
+        Log.d(TAG, "MainActivity - onResume");
     }
 
     @Override
     protected void onPause() {
+
         super.onPause();
+        Log.d(TAG, "MainActivity - onPause");
     }
 
     @Override
     protected void onRestart() {
+
         super.onRestart();
+        Log.d(TAG, "MainActivity - onRestart");
     }
 
 }
