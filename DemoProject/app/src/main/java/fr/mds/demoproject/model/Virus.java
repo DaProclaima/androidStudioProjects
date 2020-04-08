641492e0c4040761f6d22b8d1274c109be0134ab
daproclaima <sn.itpro@gmail.com>
2020-04-08 14:39:20 +0200

    app complete and connected to firebase

---
+++ DemoProject/app/src/main/java/fr/mds/demoproject/model/Virus.java
@@ -0,0 +1,47 @@
+package fr.mds.demoproject.model;
+
+public class Virus {
+
+    private String name;
+    private String countryName;
+    private int mortalityRate;
+
+
+    public Virus() {
+    }
+
+    public Virus(String name, String countryName, int mortalityRate) {
+        this.name = name;
+        this.countryName = countryName;
+        this.mortalityRate = mortalityRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getMortalityRate() {
+        return mortalityRate;
+    }
+
+    public void setMortalityRate(int mortalityRate) {
+        this.mortalityRate = mortalityRate;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
